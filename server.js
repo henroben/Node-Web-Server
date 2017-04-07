2e9ba6b1e495665fe29ca68504bee0e575fa77d7
Ben Mercer <ben@henroben.co.uk>
2017-04-07 10:34:18 +0100

    Logging and maintenance mode middleware added

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+
+// Set Maintenance Mode
+const maintenance = false;
 
 let app = express();
 
@@ -9,6 +13,31 @@ let app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 // set handlbars templating engine
 app.set('view engine', 'hbs');
+
+// Set Middleware
+
+// Logging
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.error('Unable to append to server.log');
+        }
+    });
+    next();
+});
+// check if in maintenance mode
+if(maintenance === true) {
+    app.use((req, res, next) => {
+        res.render('maintenance.hbs', {
+            pageTitle: 'Currently Undergoing Maintenance',
+            welcomeMessage: "We'll be back soon!",
+        });
+    });
+}
 // serve static pages
 app.use(express.static(__dirname + '/public'));
 
